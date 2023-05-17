033c9d3d3a18bee9e60b4c3f340c2c9f28ba61e5
lt <1406542012@qq.com>
2023-05-17 17:43:15 +0800

    HJ06 --质数因子

---
+++ sfday01/src/main/java/com/lt/sfday10/HJ06ZSYZ.java
@@ -0,0 +1,41 @@
+package com.lt.sfday10;
+
+import java.util.Scanner;
+
+//HJ06 质数因子
+public class HJ06ZSYZ {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // 素数就是质数 质数：指在一个大于1的自然数中,除了1和此整数自身外,不能被其他自然数整除的数
+        while (sc.hasNextLine()) {
+            long num = sc.nextLong();
+            StringBuffer sf = new StringBuffer();
+
+            //对于一个正整数n, 最多只有一个大于Math.sqrt(n) 的质因子  Math.sqrt(n) --对n开根号
+            //  i<=Math.sqrt(n) 等价与 i*i <= n
+            for (long i = 2; i*i <= num; ++i) {
+                while (num % i == 0) { //取模运算 == 0 可以整除
+                    sf.append(i).append(" ");
+                    num /= i;
+                }
+            }
+            //num == 1 说明一直可以整除 输入的num是有一系列质数相乘得来的   !=0 说明 最后剩余的num为一个质数
+            System.out.println(num == 1 ? sf.toString().trim(): sf.append(num).toString().trim());
+        }
+    }
+
+    //判断是否是质数
+    public static boolean isZS(Long num){ //质数只有1和本身可以被整除
+        if(num<=1) return false;
+
+        for(int i=2; i<num; ++i){
+            if(num % i == 0){ //取模运算 可以整除 余数为0
+                return false;
+            }
+        }
+        return true;
+    }
+}
