9c3b1eeb2a363e205037fb10d667da78d0b35719
lt <1406542012@qq.com>
2023-05-24 17:53:14 +0800

    HJ14 --字符串排序

--- sfday01/src/main/java/com/lt/sfday05/HJ14ZFCPX.java
+++ sfday01/src/main/java/com/lt/sfday05/HJ14ZFCPX.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 //HJ14 --字符串排序
+/*
+    给定 n 个字符串，请对 n 个字符串按照字典序排列。
+    思路: 去重升序用treeSet 不去重升序 用数组String[]或集合list 最后调用排序方法
+ */
 public class HJ14ZFCPX {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
