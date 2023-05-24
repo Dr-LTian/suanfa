e3f9c0cc7662b8c1903213b3f8d313023c422762
lt <1406542012@qq.com>
2023-05-24 17:50:48 +0800

    HJ8 --合并表记录

--- sfday01/src/main/java/com/lt/sfday05/HJ8HBBJL.java
+++ sfday01/src/main/java/com/lt/sfday05/HJ8HBBJL.java
@@ -5,6 +5,27 @@ import java.util.Scanner;
 import java.util.TreeMap;
 
 //HJ8 --合并表记录
+/*
+    数据表记录包含表索引index和数值value（int范围的正整数），请对表索引相同的记录进行合并，
+    即将相同索引的数值进行求和运算，输出按照index值升序进行输出。
+
+    先输入键值对的个数n（1 <= n <= 500）
+    接下来n行每行输入成对的index和value值，以空格隔开
+
+    eg:输入：
+        4
+        0 1
+        0 2
+        1 2
+        3 4
+
+    输出：
+        0 3
+        1 2
+        3 4
+
+    思路:借助treeMap key存索引 value 存值 treeMap 默认按key升序
+ */
 public class HJ8HBBJL {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
