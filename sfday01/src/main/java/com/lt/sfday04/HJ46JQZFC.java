30bd670d3e6d57391bdb258c0aa33dc4510a1783
lt <1406542012@qq.com>
2023-05-24 16:20:38 +0800

    HJ46 -- 截取字符串

--- sfday01/src/main/java/com/lt/sfday04/HJ46JQZFC.java
+++ sfday01/src/main/java/com/lt/sfday04/HJ46JQZFC.java
@@ -3,6 +3,10 @@ package com.lt.sfday04;
 import java.util.Scanner;
 
 //HJ46 -- 截取字符串
+
+/*
+    输入一个字符串和一个整数 k ，截取字符串的前k个字符并输出
+ */
 public class HJ46JQZFC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
