e9a44abbf7c1bdca1cd0c5d110760dd8ea46a3ec
lt <1406542012@qq.com>
2023-05-24 15:35:17 +0800

    NC68 --跳台阶

--- sfday01/src/main/java/com/lt/sfday02/NC68TTJ.java
+++ sfday01/src/main/java/com/lt/sfday02/NC68TTJ.java
@@ -1,6 +1,6 @@
 package com.lt.sfday02;
 
-//NC68--跳台阶 n阶台阶f(n) = f(n-1) + f(n-2)    f(0) = f(1) = 1
+//NC68 --跳台阶 n阶台阶f(n) = f(n-1) + f(n-2)    f(0) = f(1) = 1
 //1-1-2-3-5-8-13-21
 public class NC68TTJ {
     public static void main(String[] args) {
@@ -9,14 +9,15 @@ public class NC68TTJ {
         System.out.println("递归:" + ttj.jumpFloor2(6));
     }
 
-    public int jumpFloor(int target) {
-        //动态规划 f(n) = f(n-1) + f(n-2)
-        //定义3个变量，分别对应 f(n)、f(n-1)、f(n-2)
+    public int jumpFloor(int target) { //动态规划 :1、状态转移方程 f(n) = f(n-1) + f(n-2) 2、base case:f(0) = f(1) = 1
+        //动态规划 int[] dp = new int[n+1];   dp[n] = dp[n-1] + dp[n-2]
+        //进一步简化动态规划--》定义3个变量，分别对应 f(n)、f(n-1)、f(n-2)
         int fn = 1; //f(0) = f(1) = 1
         int fn_1 = 1;
         int fn_2 = 1;
-        //target = 0\1 时，直接返回 1，大于等于2时进入for循环
+        //target = 0或1 时，直接返回 1，大于等于2时进入for循环
         for(int i = 2; i <= target; i++){
+            //dp[i] = dp[i-1] + dp[i-2];
             //第三项开始是前两项的和,然后保留最新的两项，更新数据相加
             fn = (fn_1 + fn_2);
             fn_1 = fn_2;
@@ -27,7 +28,7 @@ public class NC68TTJ {
 
     public int jumpFloor2(int target) {
         //递归 f(n) = f(n-1) + f(n-2)
-        //target = 0\1 时，直接返回 1
+        //target = 0或1 时，直接返回 1
        if(target == 0 || target ==1)
            return 1;
         //大于等于2时进入递归
