3566581b885baac91c4c28374ee79834928c7f32
lt <1406542012@qq.com>
2023-05-24 16:01:51 +0800

    HJ23 --删除出现次数最少的字符

--- sfday01/src/main/java/com/lt/sfday03/HJ23SCCXCSZSDZF.java
+++ sfday01/src/main/java/com/lt/sfday03/HJ23SCCXCSZSDZF.java
@@ -1,9 +1,16 @@
 package com.lt.sfday03;
 
-//HJ23--删除出现次数最少的字符
-
 import java.util.*;
 
+//HJ23 --删除出现次数最少的字符
+
+/*
+    实现删除字符串中出现次数最少的字符，若出现次数最少的字符有多个，则把出现次数最少的字符都删除。
+    输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序。
+
+    思路：将字符串转char[] 通过map计算出每个字符出现的次数 拿到map的所有value找出最小值(借助集合工具类找到最小值Collections.min()方法)
+    遍历char[] 通过map将value=最小值的字符删除
+ */
 public class HJ23SCCXCSZSDZF {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
