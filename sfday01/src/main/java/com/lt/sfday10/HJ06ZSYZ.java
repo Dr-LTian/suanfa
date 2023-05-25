8432c87508338c8f9ae8e722dc2ec9b2d1fbe421
lt <1406542012@qq.com>
2023-05-25 17:58:11 +0800

    HJ06 --质数因子

--- sfday01/src/main/java/com/lt/sfday10/HJ06ZSYZ.java
+++ sfday01/src/main/java/com/lt/sfday10/HJ06ZSYZ.java
@@ -2,7 +2,24 @@ package com.lt.sfday10;
 
 import java.util.Scanner;
 
-//HJ06 质数因子
+//HJ06 --质数因子
+
+/*
+描述
+    功能:输入一个正整数，按照从小到大的顺序输出它的所有质因子（重复的也要列举）（如180的质因子为2 2 3 3 5 ）
+输入描述：
+    输入一个整数
+
+输出描述：
+    按照从小到大的顺序输出它的所有质数的因子，以空格隔开。
+
+    输入：
+        180
+    输出：
+        2 2 3 3 5
+
+  思路：从2 遍历到 根号n 大于根号n的质数因子最多只有一个 用短除法
+ */
 public class HJ06ZSYZ {
 
     public static void main(String[] args) {
@@ -17,7 +34,7 @@ public class HJ06ZSYZ {
             //对于一个正整数n, 最多只有一个大于Math.sqrt(n) 的质因子  Math.sqrt(n) --对n开根号
             //  i<=Math.sqrt(n) 等价与 i*i <= n
             for (long i = 2; i*i <= num; ++i) {
-                while (num % i == 0) { //取模运算 == 0 可以整除
+                while (num % i == 0) { //取模运算 == 0 可以整除 while相当于短除法先把2除尽
                     sf.append(i).append(" ");
                     num /= i;
                 }
