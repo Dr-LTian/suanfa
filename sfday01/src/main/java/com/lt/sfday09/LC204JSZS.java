ec3043385c9702ccd9745be9aa6932b8748d4fe4
lt <1406542012@qq.com>
2023-05-25 17:17:09 +0800

    LC204 --计数质数

--- sfday01/src/main/java/com/lt/sfday09/LC204JSZS.java
+++ sfday01/src/main/java/com/lt/sfday09/LC204JSZS.java
@@ -2,7 +2,18 @@ package com.lt.sfday09;
 
 import java.util.Arrays;
 
-//leetcode 204.计数质数 埃氏筛 枚举 线性筛
+//LC204 --计数质数 埃氏筛 枚举 线性筛
+
+/*
+    描述:
+    给定整数 n ，返回 所有小于非负整数 n 的质数的数量 。
+
+    思路1:枚举 遍历2-n 判断每个数是否为质数 为质数计数
+
+    思路2:埃氏筛 定义int数组 长度为n 给数组每个元素赋值为1
+        遍历 2-n 如果int[i] ==1; count++; 判断i*i是否大于n
+        如果不大于 从i*i开始 每次+=i 循环将所有i的倍数在int[]中标记为0
+ */
 public class LC204JSZS {
 
     public static void main(String[] args) {
